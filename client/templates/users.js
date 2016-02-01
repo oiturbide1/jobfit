9f44c33893a1e96cb8de734c6fa8ea858386bc88
RalphGorham05 <Rgorham1@gaels.iona.edu>
2016-02-01 08:09:47 -0500

    work on skills dropdown
    trying to add skills one by one

--- client/templates/users.js
+++ client/templates/users.js
@@ -1,4 +1,4 @@
-Template.register.events({
+ Template.register.events({
         'submit form': function(event) {
 
           event.preventDefault();
@@ -351,3 +351,23 @@ Template.google.events({
 
 });
 */
+
+
+
+function addElement(parentId, elementTag, elementId, html) {
+    // Adds an element to the document
+    var p = document.getElementById(parentId);
+    var newElement = document.createElement(elementTag);
+    newElement.setAttribute('id', elementId);
+    newElement.innerHTML = html;
+    p.appendChild(newElement);
+}
+
+
+var fileId = 0; // used by the addFile() function to keep track of IDs
+function addFile() {
+    fileId++; // increment fileId to get a unique ID for the new element
+    var html = '<input type="file" name="uploaded_files[]" /> ' +
+               '<a href="" onclick="javascript:removeElement('file-' + fileId + ''); return false;">Remove</a>';
+    addElement('files', 'p', 'file-' + fileId, html);
+}
