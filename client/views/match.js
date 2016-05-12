de34262dfeab95b90ff25c7ba6e0ad622a6b856a
oiturbide1 <iturbideo984@gmail.com>
2016-05-11 22:08:07 -0400

    Update match.js

--- client/views/match.js
+++ client/views/match.js
@@ -53,7 +53,7 @@ Template.matchAlg.helpers({
 				console.log(result);
 				Session.set('temp', result);
 			}
-
+				
 		});
 
 		/*
@@ -61,14 +61,14 @@ Template.matchAlg.helpers({
 			if(error)
 				console.log(error);
 			else{
-
+				
 				Session.set('temp', result);
 			}
-
+				
 		});*/
-
+		
 		return false;
-
+		
 		//return temp.length;
 	}
 
@@ -79,11 +79,11 @@ Template.matchAlg.helpers({
 Template.match.helpers({
 	'foundUser': function() {
 		if(Session.get('surv') != null){
-			//console.log(Session.get('temp').length);
-    		return Session.get('temp');
+			//console.log(Session.get('temp').length); 
+    		return Session.get('temp');	
 		}
 		return null;
-
+		
   	}
 
 });
@@ -102,7 +102,16 @@ Template.result.helpers({
 		last_list.sort(function(a,b){
 			return parseFloat(b.rank) - parseFloat(a.rank);
 		});
-		return last_list;
+		var arr = [];
+		var max;
+		if(last_list.length > 20)
+			max = 20;
+		else
+			max = last_list.length;
+		for(var k = 0; k < max; k++){
+			arr.push(last_list[k]);
+		}
+		return arr;
 		}
 });
 
@@ -119,7 +128,7 @@ Template.info.helpers({
 })
 
 Template.match.events({
-	'submit' :function(event){
+	'click' :function(event){
 		if(event.target.id == "returned"){
 			var str = event.target.innerHTML.trim();
 			var i;
@@ -130,18 +139,18 @@ Template.match.events({
 					Session.set('employerInfo', emps[i]);
 					break;
 				}
-
+				
 			}
 
-
+			
 
 		}
-		return false;
+		//return false;
 		//console.log(event.target.id);
 	}
 });
-
-
+  
+    
 
 function getRank(user, emp){
 		var rank = {emp: null, rank: 0.0};
@@ -152,7 +161,7 @@ function getRank(user, emp){
 			if(user[i] == undefined)
 				user[i] = 0;
 		}
-
+		
 		var matchAvg = 0;
 
 		matchAvg += Math.abs(user.worklife - emp.worklife);
@@ -165,14 +174,14 @@ function getRank(user, emp){
 		matchAvg += Math.abs(user.auton - emp.auton);
 		matchAvg += Math.abs(user.salary - emp.salary);
 		matchAvg += Math.abs(user.goodsup - emp.goodsup);
-		matchAvg += Math.abs(user.flex - emp.flex);
-		matchAvg += Math.abs(user.mission - emp.mission);
+		matchAvg += Math.abs(user.flex - emp.flex);			
+		matchAvg += Math.abs(user.mission - emp.mission);		
 		matchAvg += Math.abs(user.health - emp.health);
 		matchAvg += Math.abs(user.rewrecog - emp.rewrecog);
 		matchAvg += Math.abs(user.workspace - emp.workspace);
 		matchAvg += Math.abs(user.poorperfs - emp.poorperfs);
-
-
+		
+		
 		matchAvg = matchAvg / 15;
 		rank.emp = emp;
 		rank.rank = 100 - matchAvg.toFixed(2);
