2240b0e27222992d59c11d127ada7147fd749c89
oiturbide1 <iturbideo984@gmail.com>
2016-05-13 21:43:55 -0400

    Update info.js

--- client/views/info.js
+++ client/views/info.js
@@ -205,8 +205,6 @@ Template.creds.events({
       var cred1 = AutoForm.getFieldValue('creds.0','credsForm');
       var cred2 = AutoForm.getFieldValue('creds.1','credsForm');
       var cred3 = AutoForm.getFieldValue('creds.2','credsForm');
-      var cred4 = AutoForm.getFieldValue('creds.3','credsForm');
-      var cred5 = AutoForm.getFieldValue('creds.4','credsForm');
 
 
       var user = Meteor.userId;
@@ -215,10 +213,9 @@ Template.creds.events({
       // Add skill info to profile
       Meteor.users.update(
         {_id: Meteor.userId()}, {$set:
-          {"profile.credentials.creds": [ cred1, cred2, cred3, cred4, cred5] }
+          {"profile.credentials.creds": [ cred1, cred2, cred3] }
       });
 
-      Router.go('tSurvey');
 
 
     }
@@ -249,6 +246,7 @@ Template.occupationInfo.events({
       event.target.industry.value = "";
       event.target.occupation.value = '';
       event.target.jobLevel.value = '';
+      Router.go("jobInfoView");
 
     }
 
